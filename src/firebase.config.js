9e2ae0390938f75d22e0990517f42f49d36c32e8
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-08-18 13:31:31 +0600

    Project updated

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAx0UcWjn8kHfmBZv8QesSMHMxP8ka72_U",
+  authDomain: "the-kawaii-factory.firebaseapp.com",
+  projectId: "the-kawaii-factory",
+  storageBucket: "the-kawaii-factory.appspot.com",
+  messagingSenderId: "537079540786",
+  appId: "1:537079540786:web:ce972aa456105c7df0602b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
